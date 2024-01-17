01f400f42e99e697d5c0d913864e795a6d58c159
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-01-17 12:01:00 -0600

    Update Genero model with table, primary key, and fillable attributes

--- app/Models/Genero.php
+++ app/Models/Genero.php
@@ -8,5 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Genero extends Model
 {
     use HasFactory;
-    
+
+    protected $table = 'lib_generos';
+    protected $primaryKey = 'cod_genero';
+    protected $fillable = ['descripcion'];
 }
